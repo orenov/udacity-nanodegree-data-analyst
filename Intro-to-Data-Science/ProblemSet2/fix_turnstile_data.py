1e1adb3320865089a04a8bebafd8263497f0f722
orenov <orenov@codeminders.com>
2015-01-16 02:43:11 +0200

    lesson1&2

---
+++ Intro-to-Data-Science/ProblemSet2/fix_turnstile_data.py
@@ -0,0 +1,47 @@
+import csv
+import numpy
+
+def fix_turnstile_data(filenames):
+    '''
+    Filenames is a list of MTA Subway turnstile text files. A link to an example
+    MTA Subway turnstile text file can be seen at the URL below:
+    http://web.mta.info/developers/data/nyct/turnstile/turnstile_110507.txt
+    
+    As you can see, there are numerous data points included in each row of the
+    a MTA Subway turnstile text file. 
+
+    You want to write a function that will update each row in the text
+    file so there is only one entry per row. A few examples below:
+    A002,R051,02-00-00,05-28-11,00:00:00,REGULAR,003178521,001100739
+    A002,R051,02-00-00,05-28-11,04:00:00,REGULAR,003178541,001100746
+    A002,R051,02-00-00,05-28-11,08:00:00,REGULAR,003178559,001100775
+    
+    Write the updates to a different text file in the format of "updated_" + filename.
+    For example:
+        1) if you read in a text file called "turnstile_110521.txt"
+        2) you should write the updated data to "updated_turnstile_110521.txt"
+
+    The order of the fields should be preserved. 
+    
+    You can see a sample of the turnstile text file that's passed into this function
+    and the the corresponding updated file in the links below:
+    
+    Sample input file:
+    https://www.dropbox.com/s/mpin5zv4hgrx244/turnstile_110528.txt
+    Sample updated file:
+    https://www.dropbox.com/s/074xbgio4c39b7h/solution_turnstile_110528.txt
+    '''
+    fun = lambda x, n, acc=[]: fun(x[n:], n, acc+[(x[:n])]) if x else acc
+
+    for name in filenames:
+        with open(name, 'r') as in1, open("updated_"+name, 'wb') as out:
+            reader = csv.reader(in1, delimiter=',')
+            writer = csv.writer(out, delimiter=',',lineterminator='\n')
+            for row in reader:
+                common = row[:3]
+                differ = fun(row[3:],5)
+                for elem in differ:
+                    list_ = common + elem
+                    writer.writerow(list_)
+
+
